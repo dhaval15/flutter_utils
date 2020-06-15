9267e747be0c9bccd38df57bb98138caa4d058b4
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-15 23:14:43 +0530

    Breaking changes in consumer.dart

--- lib/src/arch/consumer.dart
+++ lib/src/arch/consumer.dart
@@ -2,14 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 typedef ConsumerWidgetBuilder<T> = Widget Function(
-    BuildContext context, Model<T> model);
+    BuildContext context, T state);
 
-typedef UpdateFlowDecider<T> = Future<UpdateFlow> Function(
+typedef ProducerWidgetBuilder<T> = Widget Function(
+    BuildContext context, Dispatcher<T> dispatcher);
+
+typedef RebuildActionDecider<T> = Future<RebuildAction> Function(
     BuildContext context, T oldState, T newState);
+
+typedef NotifyListener<T> = Future<bool> Function(T oldState, T newState);
 
 class Consumer<T> extends StatefulWidget {
   final ConsumerWidgetBuilder<T> builder;
-  final UpdateFlowDecider<T> flow;
+  final RebuildActionDecider<T> flow;
   final Widget child;
 
   const Consumer({Key key, this.builder, this.flow, this.child})
@@ -20,8 +25,19 @@ class Consumer<T> extends StatefulWidget {
   }
 }
 
+class Producer<T> extends StatelessWidget {
+  final ProducerWidgetBuilder builder;
+
+  const Producer({Key key, @required this.builder}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final box = Provider.of<T>(context);
+    return builder(context, box);
+  }
+}
+
 class _ConsumerState<T> extends State<Consumer<T>> {
-  Model<T> model;
+  Box<T> model;
   @override
   void initState() {
     super.initState();
@@ -35,16 +51,16 @@ class _ConsumerState<T> extends State<Consumer<T>> {
     model._remove(_onUpdate);
   }
 
-  Future<bool> _onUpdate(T old) async {
-    final flow = await widget?.flow?.call(context, old, model.state) ??
-        UpdateFlow.propogate;
+  Future<bool> _onUpdate(T oldState, T newState) async {
+    final flow = await widget?.flow?.call(context, oldState, newState) ??
+        RebuildAction.rebuild;
     switch (flow) {
-      case UpdateFlow.propogate:
+      case RebuildAction.rebuild:
         setState(() {});
         return true;
-      case UpdateFlow.skip:
+      case RebuildAction.skip:
         return true;
-      case UpdateFlow.stop:
+      case RebuildAction.stop:
         return false;
     }
     return true;
@@ -52,20 +68,20 @@ class _ConsumerState<T> extends State<Consumer<T>> {
 
   @override
   Widget build(BuildContext context) {
-    return widget?.builder?.call(context, model) ?? widget.child;
+    return widget?.builder?.call(context, model.state) ?? widget.child;
   }
 }
 
 class Provider extends InheritedWidget {
   final Map<Type, dynamic Function()> providers;
-  final Map<Type, Model> _models = Map();
+  final Map<Type, Box> _models = Map();
   Provider({this.providers, Widget child}) : super(child: child);
 
-  static Model<T> of<T>(BuildContext context) {
+  static Box<T> of<T>(BuildContext context) {
     final provider = context.findAncestorWidgetOfExactType<Provider>();
     var model = provider._models[T];
     if (model == null) {
-      model = Model<T>(provider.providers[T]());
+      model = Box<T>(provider.providers[T]());
       provider._models[T] = model;
     }
     return model;
@@ -77,42 +93,57 @@ class Provider extends InheritedWidget {
   }
 }
 
-typedef NotifyListener<S> = Future<bool> Function(S old);
-
-class Model<S> with Notify {
-  Model(this._state);
+class Box<S> with Notify, Dispatcher<S> {
+  Box(this._state);
   S _state;
   S get state => _state;
-  void setState(S state, {bool notify = true}) {
-    final old = _state;
+  set state(S state) {
     _state = state;
-    if (notify) _notify(old);
   }
 
-  void dispatch(Future<S> Function(S) action, {bool notify = true}) async {
+  @override
+  void dispatch({Future<S> Function(S) action, bool notify = true}) async {
     final oldState = _state;
-    final newState = await action(_state);
+    final newState = action != null ? await action(_state) : _state;
     _state = newState;
-    if (notify) _notify(oldState);
+    if (notify) _notify(oldState, newState);
+  }
+
+  @override
+  void dispatchMultiple(
+      {List<Future<S> Function(S)> actions, bool notify = true}) async {
+    final oldState = _state;
+    var newState = _state;
+    for (final action in (actions ?? [])) {
+      newState = await action(newState);
+    }
+    _state = newState;
+    if (notify) _notify(oldState, newState);
   }
 }
 
-mixin Notify<S> {
-  final ObserverList<NotifyListener<S>> _listeners = ObserverList();
+mixin Dispatcher<S> {
+  void dispatch({Future<S> Function(S) action, bool notify = true});
+  void dispatchMultiple(
+      {List<Future<S> Function(S)> actions, bool notify = true});
+}
 
-  void _add(NotifyListener<S> listener) {
+mixin Notify<T> {
+  final ObserverList<NotifyListener<T>> _listeners = ObserverList();
+
+  void _add(NotifyListener<T> listener) {
     _listeners.add(listener);
   }
 
-  void _remove(NotifyListener<S> listener) {
+  void _remove(NotifyListener<T> listener) {
     _listeners.remove(listener);
   }
 
-  void _notify(S old) async {
+  void _notify(T oldState, T newState) async {
     for (final listener in _listeners) {
-      if (!(await listener.call(old))) break;
+      if (!(await listener.call(oldState, newState))) break;
     }
   }
 }
 
-enum UpdateFlow { propogate, skip, stop }
+enum RebuildAction { rebuild, skip, stop }
